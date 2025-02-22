e23b2d642ddc8916e4567399e7f0cfa4139eb8fe
LiamDrew <liamdrew92@icloud.com>
2025-02-22 18:46:04 -0500

    Merging in stack branch

--- virt/main.c
+++ virt/main.c
@@ -32,6 +32,43 @@ int main(int argc, char **argv)
     printf("Block size should be 2 %u\n", a3);
     vs_free(mem, first_malloc_result);
 
+    /* Stack Tests */
+    Stack *s = stack_init(10);
+    stack_push(s, 1);
+    stack_push(s, 2);
+    stack_push(s, 3);
+    stack_push(s, 4);
+    stack_push(s, 5);
+    stack_push(s, 6);
+    stack_push(s, 7);
+    stack_push(s, 8);
+    stack_push(s, 9);
+    stack_push(s, 10);
+    stack_push(s, 11);
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+    printf("top: %d\n", stack_pop(s));
+
+    stack_free(s);
+
+    /* Recycler Tests */
+    // Mem_T mem;
+    // mem.mem = NULL;
+    // mem.recycler = recycler_init();
+
+    // assert(stack_is_empty(&((Stack*)mem.recycler)->stack));
+    // free_segment(&mem, 32);
+
+    // (void)recycler;
+
     printf("Hi there\n");
     return 0;
 }
