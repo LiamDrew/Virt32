a668ad3bf6e97f15d02d82bed2b2895568aba802
LiamDrew <liamdrew92@icloud.com>
2025-02-22 23:29:35 -0500

    Sloppy Merging the finished recycler into the kernel

--- virt/main.c
+++ virt/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdint.h>
+#include <stddef.h>
+
 #include "driver.h"
 #include "mem_state.h"
 
@@ -28,37 +30,37 @@ int main(int argc, char **argv)
     // uint32_t a2 = get_blocks_from_alloc_size(24);
     // uint32_t a3 = get_blocks_from_alloc_size(25);
 
-    // printf("Block size should be 1 %u\n", a1);
-    // printf("Block size should be 1 %u\n", a2);
-    // printf("Block size should be 2 %u\n", a3);
-    // vs_free(mem, first_malloc_result);
+    printf("Block size should be 1 %u\n", a1);
+    printf("Block size should be 1 %u\n", a2);
+    printf("Block size should be 2 %u\n", a3);
+    vs_free(mem, first_malloc_result);
 
-    /* Stack Tests */
-    Stack *s = stack_init(10);
-    stack_push(s, 1);
-    stack_push(s, 2);
-    stack_push(s, 3);
-    stack_push(s, 4);
-    stack_push(s, 5);
-    stack_push(s, 6);
-    stack_push(s, 7);
-    stack_push(s, 8);
-    stack_push(s, 9);
-    stack_push(s, 10);
-    stack_push(s, 11);
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
-    printf("top: %d\n", stack_pop(s));
+    // /* Stack Tests */
+    // Stack *s = stack_init(10);
+    // stack_push(s, 1);
+    // stack_push(s, 2);
+    // stack_push(s, 3);
+    // stack_push(s, 4);
+    // stack_push(s, 5);
+    // stack_push(s, 6);
+    // stack_push(s, 7);
+    // stack_push(s, 8);
+    // stack_push(s, 9);
+    // stack_push(s, 10);
+    // stack_push(s, 11);
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
+    // printf("top: %d\n", stack_pop(s));
 
-    stack_free(s);
+    // stack_free(s);
 
     /* Recycler Tests */
 
@@ -98,6 +100,5 @@ int main(int argc, char **argv)
 
     // (void)recycler;
 
-    printf("Hi there\n");
     return 0;
 }
