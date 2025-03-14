2e5676d9eed5a225fdb77ab34383f12820a44673
highway9807 <milogoldstein@gmail.com>
2025-03-14 13:20:57 -0400

    Changed Stack -> Stack_T
    Changed the user-defined struct typename Stack to Stack_T to conform with the standard set in the rest of the files

--- docker_shared/virt/stack.h
+++ docker_shared/virt/stack.h
@@ -8,14 +8,16 @@ typedef struct {
     uint32_t* stack;
     uint32_t size; // Potential remove this and capacity to optimize space
     uint32_t capacity;
-} Stack;
+} Stack_T;
 
-Stack* stack_init(uint32_t size);
-void stack_push(Stack *s, uint32_t elem);
-void stack_expand(Stack* s);
-uint32_t stack_pop(Stack* s);
-uint32_t stack_top(Stack *s);
-void stack_free(Stack* s);
-bool stack_is_empty(Stack *s);
+Stack_T* stack_init(uint32_t size);
+void stack_push(Stack_T *s, uint32_t elem);
+void stack_expand(Stack_T* s);
+uint32_t stack_pop(Stack_T* s);
+uint32_t stack_top(Stack_T *s);
+void stack_free(Stack_T* s);
+bool stack_is_empty(Stack_T *s);
+
+
 
 #endif 
